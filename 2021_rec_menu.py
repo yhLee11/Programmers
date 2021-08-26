7ae9756d31184d490b67884afed9c12e14ad79ab
yeonheelee <mrncd1@gmail.com>
2021-08-26 13:41:23 +0900

    2021 kakao recruitment

--- 2021_rec_menu.py
+++ 2021_rec_menu.py
@@ -13,3 +13,8 @@ def solution(orders=["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], course=[2,3
             answer+=[''.join(f) for f in counter if counter[f]==max(counter.values())]
     return sorted(answer)
 print(solution())
+"""
+combinations: 조합
+Counter: 딕셔너리 형태로 요소 개수 확인 가능
+.mostcommon(): value 개수가 가장 많은 순으로 리스트 반환
+"""
