65f2aa811003c329d20c7750f82707146b4dc52d
yeonheelee <mrncd1@gmail.com>
2021-08-29 19:52:52 +0900

    2018_kakao_friends4block

--- 2018_kakao_friends4block.py
+++ 2018_kakao_friends4block.py
@@ -1,23 +1,22 @@
-def search_pop(b,m,n):
-    pop_set=set()
-    for i in range(1,n):
-        for j in range(1,m):
-            if b[i][j]==b[i-1][j-1]==b[i-1][j]==b[i][j-1]!='_':
-                pop_set = set([(i,j),(i-1,j-1),(i-1,j),(i,j-1)])
-    for i,j in pop_set:
-        b[i][j]=0
-    for i,row in enumerate(b):
-        empty=['_']*row.count(0)
-        b[i]=empty=[block for block in row if block !=0]
-    return len(pop_set)
-
-def solution(m=6, n=6, board=["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]):
-    count=0
+def solution(m=4, n=5, board=["CCBDE", "AAADE", "AAABF", "CCBBF"]):
     b=list(map(list,zip(*board)))
+    cnt=0
+    print(b)
+    pop_set=set()
     while True:
-        pop=search_pop(b,m,n)
-        if pop==0:return count
-        count+=pop
+        pop_set=set()
+        for i in range(n-1):
+            for j in range(m-1):
+                if b[i][j]==b[i][j+1]==b[i+1][j]==b[i+1][j+1]!='_':
+                    pop_set|=set([(i,j),(i,j+1),(i+1,j),(i+1,j+1)])#합집합
+        if not pop_set:break
 
+        for i,j in pop_set:
+            b[i][j]=0
+        for i,row in enumerate(b):
+            empty=['_']*row.count(0)
+            b[i]=empty+[block for block in row if block!=0]
 
+        cnt+=len(pop_set)
+    return cnt
 print(solution())
