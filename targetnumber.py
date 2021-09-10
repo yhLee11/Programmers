f5cd2f04ca4119ab4789d2b0943772d98ba5c9a8
yeonheelee <mrncd1@gmail.com>
2021-09-10 12:29:55 +0900

    targetnumber

---
+++ targetnumber.py
@@ -0,0 +1,16 @@
+from collections import deque
+def solution(numbers, target):
+    answer = 0
+    dq=deque([(0,0)])#합과 깊이
+
+    while dq:
+        sum,level=dq.popleft()
+
+        if level > len(numbers):
+            break
+        elif level==len(numbers) and sum==target:
+            answer+=1
+        dq.append((sum+numbers[level-1],level+1))
+        dq.append((sum-numbers[level-1],level+1))
+
+    return answer
