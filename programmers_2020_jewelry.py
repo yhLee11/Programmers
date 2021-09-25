33d918281bebc8e08552dd0c41475618a63e0c43
yhLee11 <mrncd1@gmail.com>
2021-09-26 08:38:27 +0900

    jewelry

--- programmers_2020_jewelry.py
+++ programmers_2020_jewelry.py
@@ -1,12 +1,27 @@
+from collections import defaultdict
 def solution(gems):
-    answer = []
+    start,end=0,0
     kinds=list(set(gems))
-    print(kinds)
-    lst=[]
-    for g in gems:
-
-        if g in kinds:
-            lst.append(g)
-
-    return answer
-print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]))
+    num=[0]*len(kinds)
+    cnt=0
+    res=[]
+    for i in range(len(gems)):
+        num=[0]*len(kinds)
+        zero=True
+        start=i+1
+        for j in range(i,len(gems)):
+            idx=kinds.index(gems[j])
+            num[idx]+=1
+            for z in range(len(num)):
+                if num[z]==0:
+                    break
+                if z==len(num)-1 and num[z]!=0:
+                    zero=False
+                    end=j+1
+                    break
+            if z==len(num)-1 and num[z]!=0:
+                break
+        if not zero:
+            res.append((start,end))
+    res.sort(key=lambda x:(x[1]-x[0],x[0]))
+    return list(res[0])
