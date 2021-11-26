34a87426f2000c61f981e1f168d0363079a5c70a
yeonheelee <mrncd1@gmail.com>
2021-11-26 21:38:41 +0900

    N으로 표현, DP

--- representationByN.py
+++ representationByN.py
@@ -6,9 +6,23 @@ def solution(N, number):
     #dp[3]={555,(dp[1],dp[2]),(dp[2],dp[1])}
     #dp[i] set에서 number가 존재하면 i를 반환
     #발견 못하면 return -1
+    ans=-1
     dp=[]
     for i in range(1,9):
         all_case=set()
         chk_num=int(str(N)*i)
         #{N},{NN},{NNN}...
-        # for j in range(0,i-1):
+        all_case.add(chk_num)
+        for j in range(0,i-1):
+            for op1 in dp[j]:
+                for op2 in dp[-j-1]:
+                    all_case.add(op1-op2)
+                    all_case.add(op1+op2)
+                    all_case.add(op1*op2)
+                    if op2 !=0:
+                        all_case.add(op1//op2)
+        if number in all_case:
+            ans=i
+            break
+        dp.append(all_case)
+    return ans
