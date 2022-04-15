dfd7f1b4f2be250a5dd703326901a543d4a829b2
yeonheelee <mrncd1@gmail.com>
2022-04-15 10:05:16 +0900

    two pointer

--- programmers_2020_jewelry.py
+++ programmers_2020_jewelry.py
@@ -2,27 +2,29 @@ from collections import defaultdict
 def solution(gems):
     dic=defaultdict(int)
     jew=len(set(gems))
-    if jew==len(gems):
-        return list(1,len(gems))
+    gems_len=len(gems)
     ans=[]
     start,end=0,0
+
     while True:
-        if start==len(gems):
-            break
-        if end==len(gems):
-            dic[gems[end-1]]+=1
+        if end==gems_len:
+            if start==gems_len:break
             if len(dic)==jew:
-                ans.append((end-start,start,end))
-            dic=defaultdict(int)
+                ans.append([end-start,start,end])
+                dic[gems[start]]-=1
+                if dic[gems[start]]==0:
+                    del(dic[gems[start]])
             start+=1
-            end=start
-        for i in range(start,end):
-            dic[gems[i]]+=1
-        if len(dic)==jew:
-            ans.append((end-start,start,end))
-            dic=defaultdict(int)
-            start+=1
-            end=start
-        end+=1
+
+        else:
+            if len(dic)==jew:
+                ans.append([end-start,start,end])
+                dic[gems[start]]-=1
+                if dic[gems[start]]==0:
+                    del(dic[gems[start]])
+                start+=1
+            else:
+                dic[gems[end]]+=1
+                end+=1
     ans.sort(key=lambda x:(x[0],x[1]))
     return [ans[0][1]+1,ans[0][2]]
